9378ac27444ff1a3b266f67110c84d665580d9f2
rashidsarker <rashidrock558@gmail.com>
2025-05-19 13:17:59 +0600

    all done

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -34,7 +34,7 @@ axiosInstance.interceptors.response.use(
     if (error.response) {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx
-      console.error("Response Error:", error.response.data);
+      console.error("Response Errorr:", error.response.data);
     } else if (error.request) {
       // The request was made but no response was received
       console.error("Request Error:", error.request);
